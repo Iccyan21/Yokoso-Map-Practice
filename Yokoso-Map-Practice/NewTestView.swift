f06185978410708c8e5c3374d78d302e6c0a022d
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-05-18 22:30:35 +0900

    ソースつりーテスト

---
+++ Yokoso-Map-Practice/NewTestView.swift
@@ -0,0 +1,18 @@
+//
+//  NewTestView.swift
+//  Yokoso-Map-Practice
+//
+//  Created by 水原　樹 on 2024/05/18.
+//
+
+import SwiftUI
+
+struct NewTestView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewTestView()
+}
