586574faea2f61ac8e039a3c7e45b6ebe2c76a5c
MIzuhara Itsuki <iccyan0429@gmail.com>
2024-06-27 23:21:17 +0900

    SettingView修正

---
+++ Yokoso-Map-Practice/LauchScreen.swift
@@ -0,0 +1,26 @@
+//
+//  LauchScreen.swift
+//  Yokoso-Map-Practice
+//
+//  Created by 水原　樹 on 2024/06/27.
+//
+
+import SwiftUI
+
+struct LaunchScreenView: View {
+    var body: some View {
+        VStack{
+       
+                
+            Image("HomeLogo")
+                .resizable()
+                .frame(width: 400,height:  400)
+        }
+        
+    }
+}
+
+#Preview {
+    LaunchScreenView()
+}
+
